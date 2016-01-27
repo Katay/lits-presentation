6c76a6298bc44572462174ff573112e6d95600f0
vitaliy <vitalii.borodii@litslink.com>
2016-01-27 16:52:10 +0200

    adaptive fixes

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,32 @@
 $(document).on('ready', function () {
-
-
-    var menu = $('#menu');
+    var menu = $('#menu'),
+        header = $('.header'),
+        $window = $(window);
     $('#fullpage').fullpage({
         anchors: ['home', 'who_is','our_services', 'our_tech', 'our_startups', 'hit_system', 'massajor', 'wow-foto', 'why_litslink', 'contact_us'],
         menu: '#menu',
         paddingTop: 100,
         paddingBottom: 30,
-        fixedElements: '.header'
+        responsiveHeight: 750,
+        afterLoad: function (anchorLink) {
+            showHeader(anchorLink !== 'home')
+        },
+        //scrollBar: true,
+        //resize: true,
+        fixedElements: '.header, .footer'
     });
     $('button.navbar-toggle').on('click', function () {
         menu.toggleClass('menu-visible');
+    });
+    function showHeader (show) {
+        if (show) {
+            header.addClass('expanded');
+        } else {
+            header.removeClass('expanded');
+        }
+    }
+    $window.on('scroll', function () {
+        var y = window.scrollY || window.pageYOffset;
+        showHeader(!y);
     });
 });
